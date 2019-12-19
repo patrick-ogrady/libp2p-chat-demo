74c60b79ffd08cb83371a33777a3e368682c730f
Patrick O'Grady <prohb125@gmail.com>
2019-12-18 22:33:40 -0800

    Revert ws change

--- host.go
+++ host.go
@@ -29,7 +29,7 @@ func makeRoutedHost(listenPort int) (host.Host, error) {
 	}
 
 	opts := []libp2p.Option{
-		libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d/ws", listenPort)),
+		libp2p.ListenAddrStrings(fmt.Sprintf("/ip4/0.0.0.0/tcp/%d", listenPort)),
 		libp2p.Identity(priv),
 		libp2p.DefaultTransports,
 		libp2p.DefaultMuxers,
